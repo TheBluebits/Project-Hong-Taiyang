e590deaeb58de627dc565d4b0ed403ccb06d9a8d
Tim Anhalt <tim@anhalt.me>
2023-12-08 00:43:55 +0100

    Achieved correct visual BlockModel connection between logs and branches for #4

--- src/main/java/cc/bluebits/hongtaiyang/block/custom/DarkdwellerLogBlock.java
+++ src/main/java/cc/bluebits/hongtaiyang/block/custom/DarkdwellerLogBlock.java
@@ -12,14 +12,13 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-
 public class DarkdwellerLogBlock extends ModFlammableRotatedPillarBlock {
-    public static final IntegerProperty CONNECTIONS = IntegerProperty.create("connections", 0, 4);
-    public static final IntegerProperty ORIENTATION = IntegerProperty.create("orientation", 0, 3);
-    public static final BooleanProperty STRAIGHT = BooleanProperty.create("straight");
+    public static final BooleanProperty NORTH = BooleanProperty.create("north");
+    public static final BooleanProperty EAST = BooleanProperty.create("east");
+    public static final BooleanProperty SOUTH = BooleanProperty.create("south");
+    public static final BooleanProperty WEST = BooleanProperty.create("west");
+    public static final BooleanProperty ABOVE = BooleanProperty.create("above");
+    public static final BooleanProperty BELOW = BooleanProperty.create("below");
     
     public static final VoxelShape SHAPE = Block.box(4, 0, 4, 12, 16, 12);
 
@@ -33,67 +32,49 @@ public class DarkdwellerLogBlock extends ModFlammableRotatedPillarBlock {
     public DarkdwellerLogBlock(Properties pProperties) {
         super(pProperties);
         this.registerDefaultState(this.defaultBlockState()
-                .setValue(CONNECTIONS, 0)
-                .setValue(ORIENTATION, 0)
-                .setValue(STRAIGHT, false));
+                .setValue(NORTH, false)
+                .setValue(EAST, false)
+                .setValue(SOUTH, false)
+                .setValue(WEST, false)
+                .setValue(ABOVE, false)
+                .setValue(BELOW, false));
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         super.createBlockStateDefinition(builder);
-        builder.add(CONNECTIONS);
-        builder.add(ORIENTATION);
-        builder.add(STRAIGHT);
+        builder.add(NORTH, EAST, SOUTH, WEST, ABOVE, BELOW);
     }
     
     // TODO: Implement BlockState changes when neighbor is changed
-    
-    // TODO: Fix orientation rotation (c2 not working with vertical north/west)
     @Override
     public BlockState getStateForPlacement(@NotNull BlockPlaceContext pContext) {
         BlockGetter blockGetter = pContext.getLevel();
         BlockPos blockPos = pContext.getClickedPos();
         Direction.Axis mainAxis = pContext.getClickedFace().getAxis();
-        
-        ArrayList<Direction.Axis> axes = new ArrayList<>(Arrays.asList(Direction.Axis.VALUES));
-        Collections.rotate(axes, 1);
-        axes.remove(mainAxis);
 
-        ArrayList<BlockPos> blockPosList = new ArrayList<>();
-        blockPosList.add(blockPos.relative(axes.get(0), -1));
-        blockPosList.add(blockPos.relative(axes.get(1), 1));
-        blockPosList.add(blockPos.relative(axes.get(0), 1));
-        blockPosList.add(blockPos.relative(axes.get(1), -1));
-        
-        ArrayList<BlockState> blockStateList = new ArrayList<>();
-        for(BlockPos pos : blockPosList) blockStateList.add(blockGetter.getBlockState(pos));
-
-        ArrayList<Boolean> connectionList = new ArrayList<>();
-        for(int i = 0; i < blockPosList.size(); i++) {
-            boolean isStickInstance = blockStateList.get(i).getBlock() instanceof DarkdwellerStickBlock;
-            if(!isStickInstance) {
-                connectionList.add(false);
-                continue;
-            }
-            
-            boolean isConnectingFace = blockStateList.get(i).getValue(AXIS) == axes.get(i % 2);
-            connectionList.add(isConnectingFace);
-        }
-
-        int connections = 0;
-        for(boolean connection : connectionList) {
-            if(connection) connections++;
-        }
-        
-        int orientation = connectionList.indexOf(true);
-        if (orientation == -1) orientation = 0;
-        
-        boolean straight = (connectionList.get(0) && connectionList.get(2)) || (connectionList.get(1) && connectionList.get(3));
+        boolean north = attachesTo(blockGetter, blockPos, mainAxis, Direction.Axis.Z, -1);
+        boolean east = attachesTo(blockGetter, blockPos, mainAxis, Direction.Axis.X, 1);
+        boolean south = attachesTo(blockGetter, blockPos, mainAxis, Direction.Axis.Z, 1);
+        boolean west = attachesTo(blockGetter, blockPos, mainAxis, Direction.Axis.X, -1);
+        boolean above = attachesTo(blockGetter, blockPos, mainAxis, Direction.Axis.Y, 1);
+        boolean below = attachesTo(blockGetter, blockPos, mainAxis, Direction.Axis.Y, -1);
         
         return this.defaultBlockState()
                 .setValue(AXIS, mainAxis)
-                .setValue(CONNECTIONS, connections)
-                .setValue(ORIENTATION, orientation)
-                .setValue(STRAIGHT, straight);
+                .setValue(NORTH, north)
+                .setValue(EAST, east)
+                .setValue(SOUTH, south)
+                .setValue(WEST, west)
+                .setValue(ABOVE, above)
+                .setValue(BELOW, below);
+    }
+    
+    public final boolean attachesTo(BlockGetter blockGetter, BlockPos pos, Direction.Axis mainAxis, Direction.Axis branchAxis, int direction) {
+        BlockState state = blockGetter.getBlockState(pos.relative(branchAxis, direction));
+        
+        if(mainAxis == branchAxis) return false;
+        if(!(state.getBlock() instanceof DarkdwellerStickBlock)) return false;
+        return state.getValue(AXIS) == branchAxis;
     }
 }
